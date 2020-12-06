1ea0dda864f32aba17950b7c5953f56534f8a726
Matthew Barrington <edeninflames@gmail.com>
2020-12-06 12:47:36 +0000

    Day 7

---
+++ seven/ruby/seven.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+@lookup_table = {
+  "AND" => "&",
+  "OR" => "|",
+  "LSHIFT" => "<<",
+  "RSHIFT" => ">>",
+  "NOT" => "~"
+}
+
+def solve(circuit, key)
+  foo = circuit[key]
+
+  return foo if foo.is_a? Integer
+
+  if foo.size == 3
+    circuit[key][0] = solve(circuit, foo[0]) unless foo[0].is_a? Integer
+  end
+  circuit[key][-1] = solve(circuit, foo[-1]) unless foo[-1].is_a? Integer
+
+  eval(circuit[key].join(" ").gsub(/[A-Z]+/, @lookup_table))
+end
+
+def build_circuit(lines)
+  circuit = {}
+
+  lines.each do |line|
+    input, output = line.split(" -> ")
+
+    input = input.split
+
+    if input.size == 1
+      input = input[0].to_i if input[0].match(/\d+/)
+    else
+      input.each_with_index do |_, index|
+        input[index] = input[index].to_i if input[index].match?(/\d+/)
+      end
+    end
+
+    circuit[output] = input
+  end
+
+  return circuit
+end
+
+def main
+  lines = File.readlines("../input.txt", chomp: true)
+
+  circuit_one = build_circuit(lines)
+  circuit_two = build_circuit(lines)
+
+  part_one = solve(circuit_one, "a")
+
+  puts part_one
+
+  circuit_two["b"] = part_one
+
+  puts solve(circuit_two, "a")
+end
+
+main
